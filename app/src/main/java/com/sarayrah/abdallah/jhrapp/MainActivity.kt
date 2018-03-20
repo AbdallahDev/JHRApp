018aafa0ffaae1bf222b0f9aca0de54e671699b8
abdallah.sarayrah <abdallah.er11@gmail.com>
2018-03-20 12:37:46 +0200

    insert costumed action bar title

--- app/src/main/java/com/sarayrah/abdallah/jhrapp/MainActivity.kt
+++ app/src/main/java/com/sarayrah/abdallah/jhrapp/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.sarayrah.abdallah.jhrapp
 
+import android.annotation.SuppressLint
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.ActionBar
 
 class MainActivity : AppCompatActivity() {
 
+    @SuppressLint("WrongConstant")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//this code bellow to insert custom action bar title
+        supportActionBar!!.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
+        supportActionBar!!.setCustomView(R.layout.titlebar)
+
+
     }
 }
