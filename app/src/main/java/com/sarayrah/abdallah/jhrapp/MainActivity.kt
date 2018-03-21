6846990a57f058af8439bf004d996c8018571753
abdallah.sarayrah <abdallah.er11@gmail.com>
2018-03-21 11:33:34 +0200

    bottom navigation view

--- app/src/main/java/com/sarayrah/abdallah/jhrapp/MainActivity.kt
+++ app/src/main/java/com/sarayrah/abdallah/jhrapp/MainActivity.kt
@@ -1,20 +1,11 @@
 package com.sarayrah.abdallah.jhrapp
 
-import android.annotation.SuppressLint
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.design.widget.TabLayout
-import android.support.v4.app.Fragment
-import android.support.v4.app.FragmentManager
-import android.support.v4.app.FragmentPagerAdapter
 import android.support.v7.app.ActionBar
-import kotlinx.android.synthetic.main.activity_main.*
+import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
-    var adapter: FPA? = null
-
-    @SuppressLint("WrongConstant")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -23,22 +14,7 @@ class MainActivity : AppCompatActivity() {
         supportActionBar!!.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
         supportActionBar!!.setCustomView(R.layout.titlebar)
 
-        adapter = FPA(supportFragmentManager)
-        vp.adapter = adapter
 
-        vp.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(tabs))
-
-        tabs.addOnTabSelectedListener(TabLayout.ViewPagerOnTabSelectedListener(vp))
     }
 }
 
-class FPA(fm: FragmentManager) : FragmentPagerAdapter(fm) {
-    override fun getItem(position: Int): Fragment {
-        return AboutFragment()
-    }
-
-    override fun getCount(): Int {
-        return 3
-    }
-
-}
