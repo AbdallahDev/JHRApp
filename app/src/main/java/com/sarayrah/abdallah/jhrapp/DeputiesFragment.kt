b8808e0de66e3e2a27088205ea1c1ca5e98f2870
abdallah.sarayrah <abdallah.er11@gmail.com>
2018-03-21 13:47:57 +0200

    commit

--- app/src/main/java/com/sarayrah/abdallah/jhrapp/DeputiesFragment.kt
+++ app/src/main/java/com/sarayrah/abdallah/jhrapp/DeputiesFragment.kt
@@ -11,13 +11,13 @@ import android.view.ViewGroup
 /**
  * A simple [Fragment] subclass.
  */
-class AboutFragment : Fragment() {
+class DeputiesFragment : Fragment() {
 
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about, container, false)
+        return inflater.inflate(R.layout.fragment_deputies, container, false)
     }
 
 }// Required empty public constructor
--- app/src/main/java/com/sarayrah/abdallah/jhrapp/MainActivity.kt
+++ app/src/main/java/com/sarayrah/abdallah/jhrapp/MainActivity.kt
@@ -3,6 +3,8 @@ package com.sarayrah.abdallah.jhrapp
 import android.os.Bundle
 import android.support.v7.app.ActionBar
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,6 +16,30 @@ class MainActivity : AppCompatActivity() {
         supportActionBar!!.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
         supportActionBar!!.setCustomView(R.layout.titlebar)
 
+        val tr = fragmentManager.beginTransaction()
+        val obj = AboutFragment()
+        tr.replace(R.id.fc, obj)
+        tr.commit()
+
+        nv.setOnNavigationItemSelectedListener { item ->
+
+            if (item.itemId == R.id.about) {
+                val tr = fragmentManager.beginTransaction()
+                val obj = AboutFragment()
+                tr.replace(R.id.fc, obj)
+                tr.commit()
+            } else if (item.itemId == R.id.deputies) {
+                val tr = fragmentManager.beginTransaction()
+                val obj = DeputiesFragment()
+                tr.replace(R.id.fc, obj)
+                tr.commit()
+            }
+
+            item.isChecked = true
+
+            false
+
+        }
 
     }
 }
