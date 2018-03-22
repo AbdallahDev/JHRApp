db591eeb5f2201cf41b9381190b24016315c3973
abdallah.sarayrah <abdallah.er11@gmail.com>
2018-03-22 09:39:20 +0200

    commit

--- app/src/main/java/com/sarayrah/abdallah/jhrapp/AboutFragment.kt
+++ app/src/main/java/com/sarayrah/abdallah/jhrapp/AboutFragment.kt
@@ -1,8 +1,8 @@
 package com.sarayrah.abdallah.jhrapp
 
 
-import android.os.Bundle
 import android.app.Fragment
+import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -16,10 +16,11 @@ class AboutFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
+
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_about, container, false)
     }
-
 
 
 }// Required empty public constructor
--- app/src/main/java/com/sarayrah/abdallah/jhrapp/DeputiesFragment.kt
+++ app/src/main/java/com/sarayrah/abdallah/jhrapp/DeputiesFragment.kt
@@ -2,7 +2,7 @@ package com.sarayrah.abdallah.jhrapp
 
 
 import android.os.Bundle
-import android.app.Fragment
+import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
