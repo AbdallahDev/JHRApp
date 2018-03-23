70353625c0505b6cd252ce76bc3da0fb348bf57f
abdallah.sarayrah <abdallah.er11@gmail.com>
2018-03-23 20:50:45 +0200

    commit

--- app/src/main/java/com/sarayrah/abdallah/jhrapp/AboutFragment.kt
+++ app/src/main/java/com/sarayrah/abdallah/jhrapp/AboutFragment.kt
@@ -9,6 +9,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import kotlinx.android.synthetic.main.fragment_about.view.*
+import android.support.v4.view.ViewPager
+import com.sarayrah.abdallah.jhrapp.R.id.vp
 
 
 /**
@@ -29,6 +31,26 @@ class AboutFragment : Fragment() {
         view.vp.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(view.tabs))
         view.tabs.addOnTabSelectedListener(TabLayout.ViewPagerOnTabSelectedListener(view.vp))
 
+        var pagePosition = -1
+
+        view.vp.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
+
+            }
+
+            override fun onPageSelected(position: Int) {
+                pagePosition = position
+            }
+
+            override fun onPageScrollStateChanged(state: Int) {
+                if (state == ViewPager.SCROLL_STATE_DRAGGING) {
+                    if (pagePosition == 9) {
+                        RolesFragment()
+                    }
+                }
+            }
+        })
+
         return view
     }
 
@@ -40,12 +62,19 @@ class FPA(fm: android.support.v4.app.FragmentManager) : FragmentPagerAdapter(fm)
         return when (position) {
             0 -> RolesFragment()
             1 -> SessionsFragment()
-            else -> OfficeFragment()
+            2 -> OfficeFragment()
+            3 -> ExecutiveFragment()
+            4 -> SessionsFragment()
+            5 -> SessionsFragment()
+            6 -> OfficeFragment()
+            7 -> ExecutiveFragment()
+            8 -> OfficeFragment()
+            else -> ExecutiveFragment()
         }
     }
 
     override fun getCount(): Int {
-        return 3
+        return 10
     }
 
 }
