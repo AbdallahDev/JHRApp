f07883758d02742c362719a9149feb401b8bb584
abdallah.sarayrah <abdallah.er11@gmail.com>
2018-03-24 12:41:16 +0200

    commit

--- app/src/main/java/com/sarayrah/abdallah/jhrapp/DeputyInfoFragment.kt
+++ app/src/main/java/com/sarayrah/abdallah/jhrapp/DeputyInfoFragment.kt
@@ -6,26 +6,18 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import kotlinx.android.synthetic.main.fragment_deputies.view.*
 
 
 /**
  * A simple [Fragment] subclass.
  */
-class DeputiesFragment : Fragment() {
+class DeputyInfoFragment : Fragment() {
 
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_deputies, container, false)
-
-        view.khaleel_atieh.setOnClickListener {
-            Toast.makeText(activity, "jj", Toast.LENGTH_SHORT).show()
-        }
-
-        return view
+        return inflater.inflate(R.layout.fragment_deputy_info, container, false)
     }
 
 }// Required empty public constructor
--- app/src/main/java/com/sarayrah/abdallah/jhrapp/TestActivity.kt
+++ app/src/main/java/com/sarayrah/abdallah/jhrapp/TestActivity.kt
@@ -2,6 +2,8 @@ package com.sarayrah.abdallah.jhrapp
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_test.*
 
 class TestActivity : AppCompatActivity() {
 
@@ -10,5 +12,8 @@ class TestActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test)
 
+        khaleel_atieh.setOnClickListener {
+            Toast.makeText(this, "jlkj", Toast.LENGTH_SHORT).show()
+        }
     }
 }
