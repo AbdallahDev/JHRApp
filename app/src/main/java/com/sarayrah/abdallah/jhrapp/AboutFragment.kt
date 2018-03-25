3b175cd0b05dc7e56ecf1de71b7ed3fbb262eb29
abdallah.sarayrah <abdallah.er11@gmail.com>
2018-03-25 11:10:44 +0200

    commit

--- app/src/main/java/com/sarayrah/abdallah/jhrapp/AboutFragment.kt
+++ app/src/main/java/com/sarayrah/abdallah/jhrapp/AboutFragment.kt
@@ -1,16 +1,11 @@
 package com.sarayrah.abdallah.jhrapp
 
 
-import android.support.v4.app.Fragment
 import android.os.Bundle
-import android.support.design.widget.TabLayout
-import android.support.v4.app.FragmentPagerAdapter
+import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import kotlinx.android.synthetic.main.fragment_about.view.*
-import android.support.v4.view.ViewPager
-import com.sarayrah.abdallah.jhrapp.R.id.vp
 
 
 /**
