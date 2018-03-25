2a4817c28df363ae468a4796320e01d5b8e7772b
abdallah.sarayrah <abdallah.er11@gmail.com>
2018-03-25 10:04:57 +0200

    commit

--- app/src/main/java/com/sarayrah/abdallah/jhrapp/AboutFragment.kt
+++ app/src/main/java/com/sarayrah/abdallah/jhrapp/AboutFragment.kt
@@ -18,64 +18,13 @@ import com.sarayrah.abdallah.jhrapp.R.id.vp
  */
 class AboutFragment : Fragment() {
 
-    private var adapter: FPA? = null
-
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_about, container, false)
 
-        adapter = FPA(childFragmentManager)
-        view.vp.adapter = adapter
-        view.vp.currentItem = 0
-        view.vp.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(view.tabs))
-        view.tabs.addOnTabSelectedListener(TabLayout.ViewPagerOnTabSelectedListener(view.vp))
-
-        var pagePosition = -1
-
-        view.vp.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
-            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
-
-            }
-
-            override fun onPageSelected(position: Int) {
-                pagePosition = position
-            }
-
-            override fun onPageScrollStateChanged(state: Int) {
-                if (state == ViewPager.SCROLL_STATE_DRAGGING) {
-                    if (pagePosition == 9) {
-                        RolesFragment()
-                    }
-                }
-            }
-        })
-
         return view
     }
 
-
 }// Required empty public constructor
-
-class FPA(fm: android.support.v4.app.FragmentManager) : FragmentPagerAdapter(fm) {
-    override fun getItem(position: Int): android.support.v4.app.Fragment? {
-        return when (position) {
-            0 -> RolesFragment()
-            1 -> SessionsFragment()
-            2 -> OfficeFragment()
-            3 -> ExecutiveFragment()
-            4 -> SessionsFragment()
-            5 -> SessionsFragment()
-            6 -> OfficeFragment()
-            7 -> ExecutiveFragment()
-            8 -> OfficeFragment()
-            else -> ExecutiveFragment()
-        }
-    }
-
-    override fun getCount(): Int {
-        return 10
-    }
-
-}
 
--- app/src/main/java/com/sarayrah/abdallah/jhrapp/Test2Fragment.kt
+++ app/src/main/java/com/sarayrah/abdallah/jhrapp/Test2Fragment.kt
@@ -2,12 +2,10 @@ package com.sarayrah.abdallah.jhrapp
 
 
 import android.os.Bundle
-import android.support.design.widget.TabLayout
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import kotlinx.android.synthetic.main.fragment_test2.view.*
 
 
 /**
@@ -15,20 +13,11 @@ import kotlinx.android.synthetic.main.fragment_test2.view.*
  */
 class Test2Fragment : Fragment() {
 
-    private var adapter: FPA? = null
-
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_test2, container, false)
 
-        adapter = FPA(activity!!.supportFragmentManager)
-        view.vp.adapter = adapter
-
-        view.vp.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(view.tabs))
-
-
-        return view
+        return inflater.inflate(R.layout.fragment_test2, container, false)
     }
 
 }// Required empty public constructor
