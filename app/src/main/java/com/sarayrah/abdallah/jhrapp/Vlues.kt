8b2bc01249535a478a67ecf41cb4357b1dbf88b5
abdallah.sarayrah <abdallah.er11@gmail.com>
2018-03-26 10:38:20 +0200

    commit

---
+++ app/src/main/java/com/sarayrah/abdallah/jhrapp/Vlues.kt
@@ -0,0 +1,11 @@
+package com.sarayrah.abdallah.jhrapp
+
+/**
+ * Created by abdallah.sarayrah on 3/26/2018.
+ */
+//this class to store static page names, to decide which one choose from the about fragment
+class Vlues {
+    companion object {
+        var page_name = ""
+    }
+}
