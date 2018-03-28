8ae94190904e9dd4f14f493b61aaea2f3889886f
abdallah.sarayrah <abdallah.er11@gmail.com>
2018-03-28 11:11:18 +0200

    commit

--- app/src/main/java/com/sarayrah/abdallah/jhrapp/TestActivity.kt
+++ app/src/main/java/com/sarayrah/abdallah/jhrapp/TestActivity.kt
@@ -1,16 +1,12 @@
 package com.sarayrah.abdallah.jhrapp
 
-import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.widget.Toast
-import kotlinx.android.synthetic.main.activity_test.*
+import android.os.Bundle
 
 class TestActivity : AppCompatActivity() {
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test)
-
     }
 }
