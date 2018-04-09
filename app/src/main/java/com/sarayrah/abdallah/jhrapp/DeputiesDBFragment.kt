7956f3864e6541e06c5992f7ef1b1771e8b6f753
abdallah.sarayrah <abdallah.er11@gmail.com>
2018-04-09 20:28:40 +0300

    commit

--- app/src/main/java/com/sarayrah/abdallah/jhrapp/DeputiesDBFragment.kt
+++ app/src/main/java/com/sarayrah/abdallah/jhrapp/DeputiesDBFragment.kt
@@ -1,11 +1,14 @@
 package com.sarayrah.abdallah.jhrapp
 
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_deputies_db.view.*
 
 
 // TODO: Rename parameter arguments, choose names that match
@@ -19,10 +22,30 @@ private const val ARG_PARAM2 = "param2"
  */
 class DeputiesDBFragment : Fragment() {
 
+    @SuppressLint("Recycle")
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_deputies_db, container, false)
+        val view = inflater.inflate(R.layout.fragment_deputies_db, container,
+                false)
+
+        val deputiesList = ArrayList<DeputyModel>()
+
+        val obj = JHRDB(this.activity!!)
+        val db = obj.readableDatabase
+        val cursor = db.rawQuery("select name, image from deputy", arrayOf())
+        cursor.moveToFirst()
+        while (!cursor.isAfterLast) {
+            deputiesList.add(DeputyModel(cursor.getString(cursor.getColumnIndex("name")),
+                    cursor.getInt(cursor.getColumnIndex("image"))))
+            cursor.moveToNext()
+        }
+
+        val adapter = DeputiesAdapter(this.activity!!, deputiesList)
+        view.deputies_recyclerView.layoutManager = LinearLayoutManager(this.activity!!)
+        view.deputies_recyclerView.adapter = adapter
+
+        return view
     }
 
 
