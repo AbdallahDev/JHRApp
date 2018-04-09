80eb113bd5a0fb102f706f48dd118935c05a2faa
abdallah.sarayrah <abdallah.er11@gmail.com>
2018-04-09 18:24:08 +0300

    commit

---
+++ app/src/main/java/com/sarayrah/abdallah/jhrapp/DeputiesAdapter.kt
@@ -0,0 +1,20 @@
+package com.sarayrah.abdallah.jhrapp
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.ViewGroup
+
+class DeputiesAdapter(context: Context, list: ArrayList<DeputyModel>) :
+        RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
