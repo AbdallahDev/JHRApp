73e90abddfe18a6f63751ae609e13acd551d4e9e
abdallah.sarayrah <abdallah.er11@gmail.com>
2018-04-09 16:40:18 +0300

    commit

---
+++ app/src/main/java/com/sarayrah/abdallah/jhrapp/Test2Activity.kt
@@ -0,0 +1,12 @@
+package com.sarayrah.abdallah.jhrapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Test2Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test2)
+    }
+}
