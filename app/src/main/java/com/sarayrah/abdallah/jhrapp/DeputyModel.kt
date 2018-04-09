a2e54c5b567500280a54609bf6c1691626232dfb
abdallah.sarayrah <abdallah.er11@gmail.com>
2018-04-09 18:06:05 +0300

    commit

--- app/src/main/java/com/sarayrah/abdallah/jhrapp/DeputyModel.kt
+++ app/src/main/java/com/sarayrah/abdallah/jhrapp/DeputyModel.kt
@@ -1,4 +1,7 @@
 package com.sarayrah.abdallah.jhrapp
 
-class DeputyModel {
+class DeputyModel(nm: String, inf: String, img: Int) {
+    var name: String = nm
+    var info: String = inf
+    var image: Int = img
 }
