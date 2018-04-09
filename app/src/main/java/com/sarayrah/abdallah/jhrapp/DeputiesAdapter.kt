e1a1b39c2ca39115325b224de963b26a94967909
abdallah.sarayrah <abdallah.er11@gmail.com>
2018-04-09 18:35:45 +0300

    commit

--- app/src/main/java/com/sarayrah/abdallah/jhrapp/DeputiesAdapter.kt
+++ app/src/main/java/com/sarayrah/abdallah/jhrapp/DeputiesAdapter.kt
@@ -5,26 +5,29 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.deputy_row.view.*
 
-class DeputiesAdapter(context: Context, list: ArrayList<DeputyModel>) :
+class DeputiesAdapter(private var context: Context, private var list: ArrayList<DeputyModel>) :
         RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        val view = LayoutInflater.from(context).inflate(R.layout.deputy_row, parent, false)
+        val view = LayoutInflater.from(context).inflate(R.layout.deputy_row, parent,
+                false)
 
-        return
+        return DeputyView(view)
     }
 
     override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return list.size
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        (holder as DeputyView).bind(list[position].name, list[position].image)
     }
 
     class DeputyView(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        fun bind(nm: String, inf: String, img: Int) {
-            itemView.
+        fun bind(nm: String, img: Int) {
+            itemView.imageView_deputy.setImageResource(img)
+            itemView.textView_deputyName.text = nm
         }
     }
 }
