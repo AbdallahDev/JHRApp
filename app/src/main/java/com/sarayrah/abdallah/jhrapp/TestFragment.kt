6d05581e37a5f4cf2f2567324aad3220c39d57bc
abdallah.sarayrah <abdallah.er11@gmail.com>
2018-04-10 12:31:08 +0300

    commit

--- app/src/main/java/com/sarayrah/abdallah/jhrapp/TestFragment.kt
+++ app/src/main/java/com/sarayrah/abdallah/jhrapp/TestFragment.kt
@@ -6,7 +6,6 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import kotlinx.android.synthetic.main.fragment_test.view.*
 
 
 /**
@@ -20,13 +19,6 @@ class TestFragment : Fragment() {
         // Inflate the layout for this fragment
         var v = inflater.inflate(R.layout.fragment_test, container, false)
 
-        v.andre_hawari_test.setOnClickListener {
-            Values.deputy_name = "andre_hawari_body"
-            val tr = fragmentManager?.beginTransaction()
-            val obj = DeputyInfoFragment()
-            tr?.replace(R.id.fc_test, obj)
-            tr?.commit()
-        }
 
         return v
     }
