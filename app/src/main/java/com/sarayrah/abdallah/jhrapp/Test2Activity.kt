dedd4835f63eae3e1d3ad07b012a51dd4211d999
abdallah.sarayrah <abdallah.er11@gmail.com>
2018-04-10 10:24:37 +0300

    commit

--- app/src/main/java/com/sarayrah/abdallah/jhrapp/Test2Activity.kt
+++ app/src/main/java/com/sarayrah/abdallah/jhrapp/Test2Activity.kt
@@ -1,21 +1,19 @@
 package com.sarayrah.abdallah.jhrapp
 
-import android.support.v7.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
-import android.widget.Toast
+import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_test2.*
 
 class Test2Activity : AppCompatActivity() {
 
+    @SuppressLint("Recycle")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test2)
 
-        val obj = JHRDB(this)
-        val db = obj.readableDatabase
-        val cursor = db.rawQuery("select name, info, image from deputy", arrayOf())
-        cursor.moveToFirst()
 
-        textView.text = cursor.getString(cursor.getColumnIndex("info"))
+
+        textView.text = Values.deputy_info
     }
 }
