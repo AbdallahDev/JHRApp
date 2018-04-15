0bb7f9f6aa8c8c740c32e8814ac347ca69213b55
abdallah.sarayrah <abdallah.er11@gmail.com>
2018-04-15 11:39:18 +0300

    commit

--- app/src/main/java/com/sarayrah/abdallah/jhrapp/TestActivity.kt
+++ app/src/main/java/com/sarayrah/abdallah/jhrapp/TestActivity.kt
@@ -10,41 +10,6 @@ class TestActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test)
-
-        //this code bellow to insert custom action bar title, to make the title layout direction rtl
-        supportActionBar!!.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
-        supportActionBar!!.setCustomView(R.layout.titlebar)
-
-        val tr = supportFragmentManager.beginTransaction()
-        val obj = TestFragment()
-        tr.replace(R.id.fc, obj)
-        tr.commit()
-
-        nv.setOnNavigationItemSelectedListener { item ->
-            when {
-                item.itemId == R.id.about -> {
-                    val tr = supportFragmentManager.beginTransaction()
-                    val obj = AboutFragment()
-                    tr.replace(R.id.fc, obj)?.addToBackStack(AboutFragment().tag)?.commit()
-                }
-                item.itemId == R.id.deputies -> {
-                    val tr = supportFragmentManager.beginTransaction()
-                    val obj = DeputiesDBFragment()
-                    tr.replace(R.id.fc, obj)?.addToBackStack(DeputiesDBFragment().tag)?.commit()
-                }
-                item.itemId == R.id.committees -> {
-                    val tr = supportFragmentManager.beginTransaction()
-                    val obj = CommitteesFragment()
-                    tr.replace(R.id.fc, obj)?.addToBackStack(CommitteesFragment().tag)?.commit()
-                }
-            }
-
-            item.isChecked = true
-
-            false
-
-        }
-
     }
 
 }
