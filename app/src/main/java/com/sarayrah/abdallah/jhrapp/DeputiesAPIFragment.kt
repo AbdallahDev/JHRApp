6f9374723e6ed19abbc64e64f1b757ef8aa8c0ec
abdallah.sarayrah <abdallah.er11@gmail.com>
2018-04-19 11:56:31 +0300

    commit

--- app/src/main/java/com/sarayrah/abdallah/jhrapp/DeputiesAPIFragment.kt
+++ app/src/main/java/com/sarayrah/abdallah/jhrapp/DeputiesAPIFragment.kt
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.sarayrah.abdallah.jhrapp.models.DeputyAPIModel
 
 
 // TODO: Rename parameter arguments, choose names that match
@@ -22,7 +23,13 @@ class DeputiesAPIFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_deputies_api, container, false)
+        val view = inflater.inflate(R.layout.fragment_deputies_api, container,
+                false)
+
+        val deputiesList = ArrayList<DeputyAPIModel>()
+
+//        val tr = Voll
+        return view
     }
 
 
