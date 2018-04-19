738987a26d09c62d78fd4b6b1f4916c381f594f8
abdallah.sarayrah <abdallah.er11@gmail.com>
2018-04-19 11:46:48 +0300

    commit

---
+++ app/src/main/java/com/sarayrah/abdallah/jhrapp/adapters/DeputiesAPIAdapter.kt
@@ -0,0 +1,38 @@
+package com.sarayrah.abdallah.jhrapp.adapters
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.sarayrah.abdallah.jhrapp.R
+import com.sarayrah.abdallah.jhrapp.models.DeputyAPIModel
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.deputy_row.view.*
+
+class DeputiesAPIAdapter(val context: Context, private val list: ArrayList<DeputyAPIModel>) :
+        RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.deputy_row, parent,
+                false)
+
+        return DeputyView(view)
+    }
+
+    class DeputyView(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(name: String, image: String) {
+            itemView.textView_deputyName.text = name
+            Picasso.get().load("http://169.254.145.182/test${image.substring(1)}")
+                    .fit().into(itemView.imageView_deputyImage)
+        }
+
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        (holder as DeputyView).bind(list[position].name, list[position].image)
+    }
+}
---
+++ app/src/main/java/com/sarayrah/abdallah/jhrapp/models/DeputyAPIModel.kt
@@ -0,0 +1,6 @@
+package com.sarayrah.abdallah.jhrapp.models
+
+class DeputyAPIModel(nm: String, img: String) {
+    var name = nm
+    var image = img
+}
